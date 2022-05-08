d0fcebcf3fece75d0ff7ed53efc34b982893a4e2
blublu31 <59641773+blublu31@users.noreply.github.com>
2022-05-08 20:48:58 +0200

    Delete src directory

--- src/fr/limayrac/b3rpi/model/GrilleInterface.java
+++
@@ -1,7 +0,0 @@
-package fr.limayrac.b3rpi.model;
-
-//Debut de la création de l'intreface de la grille
-public interface GrilleInterface {
-    public int getHauteur();
-    public int getLargeur();
-}
